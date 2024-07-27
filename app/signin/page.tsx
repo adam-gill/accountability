afa0612d7e12993558137a7989642a8fece21b39
Daniel Bird <danielbirdpersonal@gmail.com>
2024-07-27 16:15:38 -0400

    Got rid of min width and added margin to the button on signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -9,7 +9,7 @@ const SignInPage: React.FC = () => {
                 <p className='text-light_gray text-sm mt-2'>Sign in to access your account</p>
             </div>
             <div className='input_section flex flex-col mt-2'>
-                <input id='email' type="email" placeholder='Email' className='mt-4 text-med_gray bg-dark_gray border border-med_gray p-2 rounded-lg py-2 px-4 text-sm min-w-80'/>
+                <input id='email' type="email" placeholder='Email' className='mt-4 text-med_gray bg-dark_gray border border-med_gray p-2 rounded-lg py-2 px-4 text-sm'/>
                 <input id='password' type="password" placeholder='Password' className='mt-4 text-med_gray bg-dark_gray border border-med_gray p-2 rounded-lg py-2 px-4 text-sm'/>
                 <button className='mt-4 bg-white text-almost_black py-2 px-4 rounded-lg p-2 text-sm font-medium'>Sign In</button>
                 <div className='flex justify-center items-center mt-6'>
@@ -21,7 +21,7 @@ const SignInPage: React.FC = () => {
             <div className='line border border-med_gray mt-6'></div>
             <div className='bottom_section mt-6 flex justify-between items-center'>
                 <small className='text-med_gray'>Don't have an account?</small>
-                <button className='py-2 px-4 bg-dark_gray border rounded-lg border-med_gray text-white'>Sign Up</button>
+                <button className='py-2 px-4 bg-dark_gray border rounded-lg border-med_gray text-white ml-10'>Sign Up</button>
             </div>
         </div>
     </div>
