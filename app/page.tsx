ad218e416d988321b2874fabf09896fd1cbe80ef
Ahnaf Tajwar <tajwara@bu.edu>
2024-07-28 04:55:56 -0400

    adjusted sign out button position

--- app/page.tsx
+++ app/page.tsx
@@ -33,23 +33,25 @@ export default function Home() {
   // }, [])
 
   return (
-    <div className="flex flex-col min-h-screen bg-lightblack">
-      <Container>
-        {loadingUser ? <h1>loading...</h1> : <h1 className="text-darkwhite">User: {user?.email}</h1>}
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4">
+    <div className="flex items-center justify-center min-h-screen bg-lightblack">
+      {loadingUser ? (
+        <h1 className="text-darkwhite">Loading...</h1>
+      ) : (
+        <div className="flex flex-col items-center">
+          <h1 className="text-darkwhite mb-8">User: {user?.email}</h1>
           <button
-            className="bg-appGreen rounded-xl py-1 px-4 text-black text-xl border-2 border-appGreen
-                            transition-all duration-300 ease-in-out
-                            hover:scale-105 hover:brightness-90 hover:bg-inherit hover:text-appGreen
-                            active:scale-90 active:bg-[#2bfccf20]
-                            sm:text-base
-              "
+            className="bg-appGreen rounded-xl py-3 px-8 text-black text-xl border-2 border-appGreen
+              transition-all duration-300 ease-in-out
+              hover:scale-105 hover:brightness-90 hover:bg-inherit hover:text-appGreen
+              active:scale-90 active:bg-[#2bfccf20]
+              sm:text-base
+            "
             onClick={() => signOut()}
           >
             Sign out
           </button>
-      </div>
-      </Container>
+        </div>
+      )}
     </div>
   );
 }
